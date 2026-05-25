382a950e599f6108d8b5109b5912dc2c4094bbf6
Giovanemoreira <giovane.demarque@escola.pr.gov.br>
2026-05-25 08:28:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
 
     for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
     }
 
@@ -14,3 +15,6 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-10-05"); 
+contadores[0].textContent = "Contagem regressiva";
